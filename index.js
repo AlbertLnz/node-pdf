5ffc8b62816102773bde31f928db3e08496123fd
AlbertLnz <albert2000_lanza@hotmail.com>
2024-04-04 11:17:56 +0200

    3 GET routes created

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ import express from 'express'
 const app = express()
 const PORT = 3001
 
+app.get('/', (req, res) => {
+  const queries = req.query
+  res.status(200).json({
+    status: 'success',
+    data: 'Hello World!',
+    queries // == queries: queries
+  })
+}) // EXAMPLE ROUTE: http://localhost:3001/?amount=300&price=10
+
+app.get('/generateInvoice', (_req, _res) => {
+
+})
+
+app.get('/convertFromOffice', (_req, _res) => {
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
